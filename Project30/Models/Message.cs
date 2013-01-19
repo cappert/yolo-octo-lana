c549a966c3b96dc2ddc541e6c34f1f63cc416231
Jonas Palmero <jonas@winningmark.com>
2013-01-19 14:57:31 -0800

    message mvc

---
+++ Project30/Models/Message.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project30.Models
+{
+    public class Message
+    {
+        public int MessageId { get; set; }
+        public int UserId { get; set; }
+        public String Title {get; set;}
+        public string Content { get; set; }
+        public string Type { get; set; }
+    }
+}
---
+++ Project30/Models/MessageEntities.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Project30.Models
+{
+    public class MessageEntities : DbContext
+    {
+        public DbSet<Message> Messages { get; set; }
+
+    }
+}
