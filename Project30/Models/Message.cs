66f2aaea4a12ceaa18bf81655e8b35fe3bf35ea6
Camille Appert <camille@appert.me>
2013-01-27 19:20:17 +0100

    add Message model

---
+++ Project30/Models/Message.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Project30.Models
+{
+
+    public class MessageContext : DbContext
+    {
+        public MessageContext() : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<Message> Messages { get; set; }
+
+    }
+
+
+    [Table("Message")]
+    public class Message
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int MessageId { get; set; }
+        /// <summary>
+        /// foreign key is virtual
+        /// </summary>
+        // public virtual UserProfile UserProfile { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Type { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
